41f25c24507f1ce80162bc7501c244a655289a24
jonscafe <68538183+jonscafe@users.noreply.github.com>
2024-02-06 20:08:59 +0700

    Update Proyek_Streamlit.py

--- Proyek_Streamlit.py
+++ Proyek_Streamlit.py
@@ -15,13 +15,6 @@
 
 # ## Import Semua Packages/Library yang Digunakan
 
-# In[1]:
-
-import subprocess
-
-# Assuming your requirements.txt is in the same directory as your script
-subprocess.run(["pip", "install", "-r", "requirements.txt"])
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn import tree
@@ -49,12 +42,6 @@ for dirname, _, filenames in os.walk('dataset/'):
         print(os.path.join(dirname, filename))
 
 
-# ## Data Wrangling
-
-# ### Gathering Data
-
-# In[2]:
-
 st.title("Spotify Chart: Song from 60's til 2023")
 
 hits_dataset_filenames=['dataset-of-10s','dataset-of-00s','dataset-of-90s','dataset-of-80s','dataset-of-70s','dataset-of-60s']
@@ -70,11 +57,6 @@ for name in hits_dataset_filenames:
     hits_dataset_dict[name]=df
 
 
-# ### Assessing Data
-
-# In[3]:
-
-
 full_hits_dataset = pd.concat(hits_dataset_dict.values(), ignore_index=True, axis=0)
 track_artist_fhd=full_hits_dataset.pop('track') +full_hits_dataset.pop('artist')
 track_artist_fhd.to_csv('dataset/track_artist_fhd.csv')
@@ -85,24 +67,13 @@ top_songs_views_dataset=top_songs_dataset.pop('streams')
 print(top_songs_dataset.columns)
 top_songs_dataset.drop(top_songs_dataset.iloc[:, 2:13], inplace=True, axis=1)
 # top_songs_dataset.drop(['track_id','duration_ms'], inplace=True, axis=1)
-
 # top_songs_dataset.columns
 
 
-# In[4]:
-
-
 most_songs_dataset=pd.read_csv('dataset/spotify_songs.csv')
-
 most_songs_dataset.drop(most_songs_dataset.iloc[:, 3:11], inplace=True, axis=1)
 most_songs_dataset.drop(['track_id','duration_ms','loudness'], inplace=True, axis=1)
-
-
-
 most_songs_dataset
-
-
-# In[5]:
 
 
 for l in most_songs_dataset.columns:
@@ -116,21 +87,10 @@ for l in most_songs_dataset.columns:
         
 track_artist_msd=most_songs_dataset.pop('track') +" "+ most_songs_dataset.pop('artist') 
 track_artist_msd.to_csv('dataset/track_artist_msd.csv')
-
 # track_artist_msd
  
-
-
-# In[6]:
-
-
 features=['tempo','key','mode','danceability','valence','energy','acousticness','instrumentalness','liveness','speechiness']
 
-
-# In[7]:
-
-
-#no loudness in this
 for l in top_songs_dataset.columns:
     if l not in full_hits_dataset.columns:
         print(l)
@@ -146,22 +106,10 @@ track_artist_tsd.to_csv('dataset/track_artist_tsd.csv')
 top_songs_views_dataset=pd.concat([track_artist_tsd,top_songs_views_dataset], axis=1, join="inner",ignore_index=True)
 top_songs_views_dataset=top_songs_views_dataset.set_axis(['track - artist', 'views'],axis='columns')
 top_songs_views_dataset.to_csv('dataset/top_songs_views_dataset.csv')
-
 # top_songs_views_dataset     
 # top_songs_views_dataset.columns
 
-
-# ### Cleaning Data
-
-# In[8]:
-
-
 chords=['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-
-
-# In[9]:
-
-
 top_songs_dataset.loc[top_songs_dataset['mode'] == 'Major', 'mode'] = 1
 top_songs_dataset.loc[top_songs_dataset['mode'] == 'Minor', 'mode'] = 0
 top_songs_dataset['key'] = top_songs_dataset['key'].fillna(-1)
@@ -173,7 +121,6 @@ for lab in labs:
     print(top_songs_dataset[lab].unique())
 
 
-# In[10]:
 
 
 feat_dict={}
@@ -193,12 +140,6 @@ top_songs_dataset=top_songs_dataset[most_songs_dataset.columns]
 
 
 # ## Exploratory Data Analysis (EDA)
-
-# ### Explore ...
-
-# In[11]:
-
-
 from sklearn.model_selection import train_test_split
 
 y=full_hits_dataset.pop('target')
@@ -209,20 +150,11 @@ full_hits_dataset.to_csv('dataset/full_hits_dataset.csv')
 
 X_train, X_test, y_train, y_test = train_test_split(full_hits_dataset, y, test_size=0.2, random_state=42)
 
-
-# ## Visualization & Explanatory Analysis
-
-# ### Pertanyaan 1:
-
-# In[17]:
-
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Assuming you already have the dataframes full_hits_dataset, top_songs_dataset, most_songs_dataset
 
 # Convert the dataframes to 1D arrays for scatter plot
 full_hits_flat = full_hits_dataset.to_numpy().flatten()
@@ -235,27 +167,16 @@ fig, axes = plt.subplots(3, 1, figsize=(10, 15))
 # Scatter plot for full_hits_dataset
 axes[0].scatter(range(len(full_hits_flat)), full_hits_flat, alpha=0.5)
 axes[0].set_title('full_hits_dataset after multiplication')
-
 # Scatter plot for top_songs_dataset
 axes[1].scatter(range(len(top_songs_flat)), top_songs_flat, alpha=0.5)
 axes[1].set_title('top_songs_dataset after multiplication')
-
 # Scatter plot for most_songs_dataset
 axes[2].scatter(range(len(most_songs_flat)), most_songs_flat, alpha=0.5)
 axes[2].set_title('most_songs_dataset after multiplication')
-
 plt.tight_layout()
-
 # Save the plot to a temporary file
 temp_file_path = "temp_scatter_plot.png"
 plt.savefig(temp_file_path, format="png")
-
-
-
-
-# ### Pertanyaan 2:
-
-# In[18]:
 
 
 def plot_feature_by_category(dataset, title):
@@ -269,21 +190,11 @@ def plot_feature_by_category(dataset, title):
 
 # Plot for full_hits_dataset
 plot_feature_by_category(full_hits_dataset, 'Popularity of Music - Full Hits Dataset')
-
 # Plot for top_songs_dataset
 plot_feature_by_category(top_songs_dataset, 'Popularity of Music - Top Songs Dataset')
-
 # Plot for most_songs_dataset
 plot_feature_by_category(most_songs_dataset, 'Popularity of Music - Most Songs Dataset')
 
-
-# ## Membuat Model 
-
-# In[19]:
-
-
-# GaussianProcessClassifier() too much memory can't run
-# KNeighborsClassifier(),RadiusNeighborsClassifier() no time to try
 
 models=[svm.SVC(),svm.NuSVC()]
 for model in models:
@@ -298,14 +209,9 @@ for model in models:
     print(conf_mat.ravel())
 
 
-# In[20]:
-
-
 # HistGradientBoostingClassifier(),RandomForestClassifier(),ExtraTreesClassifier(),GradientBoostingClassifier(),AdaBoostClassifier() best
 # svm.LinearSVC() very good not consistent
 #tree.DecisionTreeClassifier(),SGDClassifier() +
-
-
 models=[HistGradientBoostingClassifier(),RandomForestClassifier(),ExtraTreesClassifier(),GradientBoostingClassifier(),AdaBoostClassifier(),svm.LinearSVC(),SGDClassifier(),tree.DecisionTreeClassifier()]
 for model in models:
     dec = model
@@ -319,11 +225,6 @@ for model in models:
     print(conf_mat.ravel())
 
 
-# ### Training Model
-
-# In[21]:
-
-
 pred=[]
 models=[HistGradientBoostingClassifier(),RandomForestClassifier()]
 # models=[HistGradientBoostingClassifier()]
@@ -332,13 +233,8 @@ for model in models:
     dec=dec.fit(X_train,y_train)
     y_pred=dec.predict(most_songs_dataset)
     pred.append(y_pred)
-
 # full_msd_hgbc=track_artist_msd+most_songs_dataset+y_pred[0]
 # full_msd_rfc=track_artist_msd+most_songs_dataset+y_pred[1]
-
-
-# In[24]:
-
 
 pred[0]=pd.DataFrame(pred[0],columns=['hit'])
 pred[1]=pd.DataFrame(pred[1],columns=['hit'])
@@ -352,21 +248,10 @@ for model in models:
     y_pred=dec.predict(top_songs_dataset)
     preds.append(y_pred)
 
-
-# In[25]:
-
-
 preds[0]=pd.DataFrame(pred[0],columns=['hit'])
 preds[1]=pd.DataFrame(pred[1],columns=['hit'])
 
-
-# ### Evaluasi Model
-
-# In[26]:
-
-
 # result = pd.concat([df1, df4], axis=1, join="inner")
-
 full_msd_hgbc=pd.concat([track_artist_msd,pred[0]], axis=1, join="inner",ignore_index=True)
 full_msd_rfc=pd.concat([track_artist_msd,pred[1]], axis=1, join="inner",ignore_index=True)
 # full_msd_hgbc.rename(columns={"0": "track - artist", "1": "Hit"},inplace=True)
@@ -379,11 +264,7 @@ print(full_msd_hgbc['track - artist'].isin(top_songs_views_dataset['track - arti
 full_msd_rfc['track - artist'].isin(top_songs_views_dataset['track - artist'].values).unique())
 
 
-# In[27]:
-
-
 # result = pd.concat([df1, df4], axis=1, join="inner")
-
 full_tsd_hgbc=pd.concat([track_artist_tsd,preds[0]], axis=1, join="inner",ignore_index=True)
 full_tsd_rfc=pd.concat([track_artist_tsd,preds[1]], axis=1, join="inner",ignore_index=True)
 # full_msd_hgbc.rename(columns={"0": "track - artist", "1": "Hit"},inplace=True)
@@ -394,11 +275,6 @@ full_tsd_rfc=full_tsd_rfc.set_axis(['track - artist', 'hit'], axis='columns')
 # full_msd_hgbc.compare(full_msd_rfc)
 print(full_tsd_hgbc['track - artist'].isin(top_songs_views_dataset['track - artist'].values).unique(),
 full_tsd_rfc['track - artist'].isin(top_songs_views_dataset['track - artist'].values).unique())
-
-
-# ### Menyimpan Model
-
-# In[29]:
 
 
 full_msd_rfc.set_axis(['track - artist', 'hit'],axis='columns')
@@ -424,12 +300,8 @@ merged_tsd_rfc['hit'].value_counts(),merged_tsd_rfc.shape)
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Assuming merged_tsd_rfc is a DataFrame
-# If it's a numpy array, you can convert it to a DataFrame using pd.DataFrame()
-
 numeric_values = pd.to_numeric(merged_tsd_rfc.to_numpy().flatten(), errors='coerce')
 
-# Function to generate the scatter plot
 def generate_scatter_plot():
     plt.figure(figsize=(10, 6))
     plt.scatter(range(len(numeric_values)), numeric_values, alpha=0.5)
@@ -440,11 +312,6 @@ def generate_scatter_plot():
 
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Assuming merged_tsd_rfc is a DataFrame
-# If it's a numpy array, you can convert it to a DataFrame using pd.DataFrame()
-
-# Convert values to numeric
 numeric_values = pd.to_numeric(merged_tsd_rfc.to_numpy().flatten(), errors='coerce')
 
 # Create a scatter plot
@@ -456,11 +323,6 @@ plt.ylabel('Values')
 plt.show()
 
 
-# ## Conclusion
-
-# In[55]:
-
-
 def plot_feature_by_category(dataset, title):
     plt.figure(figsize=(12, 6))
     sns.barplot(data=dataset.mean().reset_index(), x='index', y=0)
@@ -470,13 +332,8 @@ def plot_feature_by_category(dataset, title):
     plt.xticks(rotation=45)
     plt.show()
 
-# In[59]:
-
 import streamlit as st
 
-# Assuming you have the dataframes merged_msd_rfc and merged_tsd_rfc
-
-    # Display Most Song Dataset
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", ['Dataset Analysis', 'Scatter Plot'])
 
@@ -484,33 +341,25 @@ if selection == 'Dataset Analysis':
     st.title('Dataset Analysis')
     st.write("Most Song Dataset")
     st.dataframe(merged_msd_rfc)
-    # Display Top Song Dataset
     st.write("Top Song Dataset")
     st.dataframe(merged_tsd_rfc)
     
 elif selection == 'Scatter Plot':
-    # Page 2: Scatter Plot
     st.header("Scatter Plot Page")
-# Button to switch to the scatter plot page
     if st.button("Show Scatter Plot"):
         st.pyplot(generate_scatter_plot())
-    # Button to show scatter plot for full_hits_dataset
     if st.button("Show Scatter Plot for full_hits_dataset"):
         generate_scatter_plot(full_hits_flat, "full_hits_dataset after multiplication")
         st.pyplot()
-    # Button to show scatter plot for top_songs_dataset
     if st.button("Show Scatter Plot for top_songs_dataset"):
         generate_scatter_plot(top_songs_flat, "top_songs_dataset after multiplication")
         st.pyplot()
-    # Button to show scatter plot for most_songs_dataset
     if st.button("Show Scatter Plot for most_songs_dataset"):
         generate_scatter_plot(most_songs_flat, "most_songs_dataset after multiplication")
         st.pyplot()
 
     plot_feature_by_category(full_hits_dataset, 'Popularity of Music - Full Hits Dataset')
-    # Plot for top_songs_dataset
     plot_feature_by_category(top_songs_dataset, 'Popularity of Music - Top Songs Dataset')
-    # Plot for most_songs_dataset
     plot_feature_by_category(most_songs_dataset, 'Popularity of Music - Most Songs Dataset')
 
 
